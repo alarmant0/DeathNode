0ff3a9e673f16a3680b7ff54b653c59cd35778cf
David <80123928+alarmant0@users.noreply.github.com>
2025-12-18 22:42:34 +0000

    fix(node): default gateway URL to https://10.0.2.10:443

--- setup_scripts/node-start.sh
+++ setup_scripts/node-start.sh
@@ -6,4 +6,6 @@ NODE_PASS="${NODE_PASS:-alice12}"
 export DEATHNODE_TLS_TRUSTSTORE_PATH="$HOME/certs/truststore.p12"
 export DEATHNODE_TLS_TRUSTSTORE_PASSWORD="$NODE_PASS"
 
+export DEATHNODE_GATEWAY_URL="${DEATHNODE_GATEWAY_URL:-https://10.0.2.10:443}"
+
 mvn -DskipTests compile exec:java
