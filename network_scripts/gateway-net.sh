467e4845b2e34fb77edf2d2997fc43cddb8d782c
David <80123928+alarmant0@users.noreply.github.com>
2025-12-19 20:38:58 +0000

    Add firewalls rules to each vm

--- network_scripts/gateway-net.sh
+++ network_scripts/gateway-net.sh
@@ -49,3 +49,32 @@ ip a show "$DMZ_IFACE"
 ip a show "$INT_IFACE"
 ip a show "$NAT_IFACE" || true
 ip route
+
+sysctl -w net.ipv4.ip_forward=1
+echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
+
+iptables -F
+iptables -X
+iptables -t nat -F
+iptables -t nat -X
+iptables -P INPUT DROP
+iptables -P FORWARD DROP
+iptables -P OUTPUT ACCEPT
+
+iptables -A INPUT -i lo -j ACCEPT
+
+iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+iptables -A FORWARD -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+
+iptables -A INPUT -p tcp -s 10.0.2.0/24 --dport 443 -m conntrack --ctstate NEW -j ACCEPT
+
+iptables -A OUTPUT -p tcp -d 10.0.1.20 --dport 443 -m conntrack --ctstate NEW -j ACCEPT
+
+iptables -A FORWARD -p tcp -s 10.0.2.0/24 -d 10.0.1.20 --dport 443 -m conntrack --ctstate NEW -j ACCEPT
+
+iptables -t nat -A POSTROUTING -o "$NAT_IFACE" -j MASQUERADE
+
+iptables -A FORWARD -i "$INT_IFACE" -o "$NAT_IFACE" -s 10.0.2.0/24 -m conntrack --ctstate NEW,ESTABLISHED,RELATED -j ACCEPT
+iptables -A FORWARD -i "$NAT_IFACE" -o "$INT_IFACE" -d 10.0.2.0/24 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+
+iptables-save > /etc/iptables/rules.v4 2>/dev/null || echo "Warning: Could not save iptables rules"
